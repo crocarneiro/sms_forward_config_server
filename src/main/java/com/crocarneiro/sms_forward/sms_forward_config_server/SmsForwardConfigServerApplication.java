331d6c6bb5eb0c38423a7520b13c5618fe629041
Carlos Rafael de Oliveira Carneiro <contato@carlos.carneiro.nom.br>
2022-07-22 23:29:21 -0300

    First commit

---
+++ src/main/java/com/crocarneiro/sms_forward/sms_forward_config_server/SmsForwardConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.crocarneiro.sms_forward.sms_forward_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsForwardConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsForwardConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crocarneiro/sms_forward/sms_forward_config_server/SmsForwardConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crocarneiro.sms_forward.sms_forward_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsForwardConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
